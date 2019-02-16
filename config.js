455171e5f3194ab31c0ae9014c1d1f07a82b737f
Blazer178 <nathan.jones01@yahoo.com>
2019-02-16 14:06:06 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,5 @@
-exports.TOKEN = "NTQ1NzM1MjY5NDUwNzc2NTc2.D0lVSw.GBsrVnWZQe5XILpz1ytHYH35cWw";
+exports.TOKEN = process.env.TOKEN;
 
-exports.GOOGLE_API_KEY = "AIzaSyBofPE6vvdAaVzM-qSVkkCnkiqDqLOMbT0";
-
-//exports.TOKEN = process.env.TOKEN;
-
-//exports.GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+exports.GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
 
 //exports.DBL_KEY - ""
