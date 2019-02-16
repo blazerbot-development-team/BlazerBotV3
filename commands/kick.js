5fa3f3770aab26a2b83c55b270fd0fe2bd06acb6
Blazer178 <nathan.jones01@yahoo.com>
2019-02-16 16:15:57 -0600

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js");
 module.exports.run = async (client,message,args) => {
 	let usr = message.content.split(' ').slice(1, 2).join(' ');
 
- if(!message.member.hasPermission("KICK_MEMBERS") && message.author.id !== "377271843502948354")return message.reply("you have to have the `KICK_MEMBERS` permission to use this command");
+ if(!message.member.hasPermission("KICK_MEMBERS") && message.author.id !== "139213278571134977")return message.reply("You have to have the `KICK_MEMBERS` permission to use this command");
   let wmem = message.mentions.members.first()||message.guild.members.get(usr);
   if(!wmem) return message.reply("I couldn't find that member");
   let reason = message.content.split(' ').slice(2).join(' ');
