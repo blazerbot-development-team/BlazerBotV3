d6abaefe503cbf668387a0f22e9913f03a16ded1
Blazer178 <nathan.jones01@yahoo.com>
2019-02-16 14:48:42 -0600

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,13 +4,13 @@ module.exports.run = async (client,message,args) => {
 	
 const uwu = message.content.split(" ").slice(1).join(" ");
 
-if (!uwu && message.author.id !== "377271843502948354") {	
+if (!uwu && message.author.id !== "139213278571134977") {	
     const RichEmbed = new Discord.RichEmbed()
     .setColor(`${message.member.displayHexColor}`)
     .setTimestamp()
     .setThumbnail(client.user.avatarURL)
     .setTitle(`help for ${client.user.username}`)
-    .setDescription(`\nwe need [Beta Testers](https://discordapp.com/invite/SQFbf9q) <= join and ask to be a beta tester`)
+    .setDescription(`\nWe need [Beta Testers](https://discordapp.com/invite/SQFbf9q) <= Join and ask to be a beta tester!`)
     .addField('**Misc commands:**', '`ping` `glitch` `emote` `remindme` `anime` `weather` `invite` `role` `sayd`')
     .addField('**Image commands:**', '`neko` `rem` `animepic` `nsfw` `lizard` `dog` `bird` `bunny` `pat` `pout` `cry` `hug` `kiss` `OwO` `avatar` `servericon`')
 	.addField('**Currency commands:**', 'DISABLED ATM')
@@ -19,15 +19,15 @@ if (!uwu && message.author.id !== "377271843502948354") {
     .addField('**Music commands:**', '`play` `skip` `stop` `pause` `resume` `volume` `np` `queue`')
     .setFooter(`Requested by: ${message.member.displayName}`, `${message.author.avatarURL}`)
     message.channel.send({embed: RichEmbed});
-    console.log(`help command has been used by ${message.author.username} in ${message.channel.guild}`);
+    console.log(`Help command has been used by ${message.author.username} in ${message.channel.guild}`);
 }
-if (!uwu && message.author.id == "377271843502948354") {	
+if (!uwu && message.author.id == "139213278571134977") {	
     const RichEmbed = new Discord.RichEmbed()
     .setColor(`${message.member.displayHexColor}`)
     .setTimestamp()
     .setThumbnail(client.user.avatarURL)
     .setTitle(`help for ${client.user.username}`)
-    .setDescription(`\nwe need [Beta Testers](https://discordapp.com/invite/SQFbf9q) <= join and ask to be a beta tester`)
+    .setDescription(`\nWe need [Beta Testers](https://discordapp.com/invite/SQFbf9q) <= Join and ask to be a beta tester`)
     .addField('**Misc commands:**', '`ping` `dbot` `glitch` `emote` `remindme` `anime` `weather` `role` `sayd`')
     .addField('**Image commands:**', '`neko` `rem` `animepic` `nsfw` `lizard` `dog` `bird` `bunny` `pat` `pout` `cry` `hug` `kiss` `OwO` `avatar` `servericon`')
 	.addField('**Currency commands:**', 'DISABLED ATM')
@@ -37,7 +37,7 @@ if (!uwu && message.author.id == "377271843502948354") {
 	.addField('**Music commands:**', '`play` `skip` `stop` `pause` `resume` `volume` `np` `queue`')
     .setFooter(`Requested by: ${message.member.displayName}`, `${message.author.avatarURL}`)
     message.channel.send({embed: RichEmbed});
-    console.log(`help command has been used by ${message.author.username} in ${message.channel.guild}`);
+    console.log(`Help command has been used by ${message.author.username} in ${message.channel.guild}`);
 }
 
 }
