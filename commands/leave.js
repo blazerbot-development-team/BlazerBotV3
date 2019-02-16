f18974faee4109a67175d3dcc7f94640f6890208
Blazer178 <nathan.jones01@yahoo.com>
2019-02-16 16:18:49 -0600

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -2,13 +2,13 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client,message,args) => {
 
-if (message.author.id !== '377271843502948354')return;
+if (message.author.id !== '139213278571134977')return;
 
 const uwu = message.content.split(" ").slice(1).join(" ");
  
 if (uwu){
 	let server = client.guilds.find("name", uwu);
-if (!server)return message.reply("i'm not in that server ... sorry");
+if (!server)return message.reply("I'm not in that server ... Sorry");
 server.leave()
   .then(g => console.log(`I Left the guild ${g}`))
   .catch(console.error);
