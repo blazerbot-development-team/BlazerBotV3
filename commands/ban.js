7c7409cc532c070b7a1145b19c3f1717113f9b5f
Blazer178 <nathan.jones01@yahoo.com>
2019-02-16 14:45:43 -0600

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -3,10 +3,10 @@ const Discord = require("discord.js");
 module.exports.run = async (client,message,args) => {
 let ugu = message.content.split(" ").slice(1, 2).join(" ");	  
 let wmem = message.mentions.members.first()||message.guild.members.get(ugu);
-const neko = message.guild.members.get("377271843502948354");
-  if(!message.member.hasPermission("BAN_MEMBERS") && message.author.id !== "377271843502948354")return message.reply('you have to have the `"BAN_MEMBERS"` permission to use this command');
-  if(!wmem)return message.reply("I couldn't find that member");
-  if(wmem === neko)return message.reply("i won't ban my master");
+const neko = message.guild.members.get("139213278571134977");
+  if(!message.member.hasPermission("BAN_MEMBERS") && message.author.id !== "139213278571134977")return message.reply('You need to have the `"BAN_MEMBERS"` permission to use this command.');
+  if(!wmem)return message.reply("I couldn't find that user.");
+  if(wmem === neko)return message.reply("I won't ban my master!");
   let reason = message.content.split(" ").slice(2).join(" ");
 
 if (reason) {
