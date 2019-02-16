a2ee609873c3a67993b0dfe8d64f3b48f3dba968
Blazer178 <nathan.jones01@yahoo.com>
2019-02-16 15:25:58 -0600

    Update setinv.js

--- commands/setinv.js
+++ commands/setinv.js
@@ -2,12 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client,message,args) => {
 	
-	var neko = message.guild.members.find("id", "377271843502948354");
-	if (message.member !== neko)return message.channel.send("you do not have the proper permissions to use this command");
+	var neko = message.guild.members.find("id", "139213278571134977");
+	if (message.member !== neko)return message.channel.send("You do not have the proper permissions to use this command");
 	if (neko) {
 	client.user.setStatus("invisible");
 	message.channel.send('My Master set my status to: ``"invisible"``');
-	console.log('you set my status to invisible');
+	console.log('You set my status to invisible');
 	}
 }
 
